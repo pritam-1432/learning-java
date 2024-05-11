3b59c10756e3a70fde963b408d7287fd72ce10ac
pritam-1432 <you@example.com>
2024-05-11 20:16:25 +0530

    how to print

---
+++ BASIC/First.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("LOVE");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age");
+        int x=sc.nextInt();
+    System.out.println("Your age is "+ x);
+    }
+}
